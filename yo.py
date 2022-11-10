c20b72c59ffee0ea97592ff5f2610ec03c101f39
Amruth Ashok <BL.EN.U4AIE20002@bl.students.amrita.edu>
2022-11-10 21:02:31 +0530

    Update yo.py

--- yo.py
+++ yo.py
@@ -1,17 +1,35 @@
-## TO START ZOOKEEPER AND KAFKA SERVER
-bin/zookeeper-server-start.sh config/zookeeper.properties
-bin/kafka-server-start.sh config/server.properties
 
+=================================. INSTRUCTIONS .==================================== 
+(Basic Starting of Kafka)
+
+## START ZOOKEEPER SERVER AND KAFKA SERVER
+
+zookeeper-server-start /Users/amruthashok/Desktop/BigData/kafka_2.13-3.3.1/config/zookeeper.properties 
+
+kafka-server-start /Users/amruthashok/Desktop/BigData/kafka_2.13-3.3.1/config/server.properties 
 
 ## TO CREATE A TOPIC
-bin/kafka-topics.sh --create -bootstrap-server localhost:9092 --topic weather
+kafka-topics --create -bootstrap-server localhost:9092 --topic weather
 
 ## TO LIST PRESENT TOPICS
-bin/kafka-topics.sh --list --bootstrap-server localhost:9092
+kafka-topics --list --bootstrap-server localhost:9092
 
+## START KAFKA CONSUMER
+kafka-console-consumer --bootstrap-server localhost:9092 --topic weather --from-beginning
 
-## START A CONSUMER
-bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic weather --from-beginning
+=======================================================================================
+
+(Wrapping Scala file in set and executing it)
+
+->Project
+    -> StreamHandler
+       ->src
+         ->main
+	  ->scala
+	   ->StreamHandler.scala
+		    
+    -> iot_devices.ipynb
+
 
 
 ## Define .scala file
@@ -19,7 +37,8 @@ bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic weather 
 
 # open a new terminal (get to the desired path which contains the above mentioned scala and sbt files:
  here it is StreamHandler folder)
+
 sbt package (will take some time)
 a .jar file will be created in target/scala-2.12/ (this jar file is to be executed)
 
-spark-submit --class StreamHandler --master local[*] --packages "org.apache.kafka-0-10_2.11:3.3.1" target/scala-2.12/streamhandler_2.12-0.1.0-SNAPSHOT.jar
+spark-submit --class StreamHandler --master "local[*]" --packages "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.1" target/scala-2.12/streamhandler_2.12-0.1.0-SNAPSHOT.jar
