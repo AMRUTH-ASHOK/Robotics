5ff47eecbe307c9919e14aa89ce7cafab909b1e8
Amruth Ashok <BL.EN.U4AIE20002@bl.students.amrita.edu>
2022-11-10 14:44:01 +0000

    Update yo.py

--- yo.py
+++ yo.py
@@ -1,31 +1,25 @@
-from kafka import KafkaConsumer
-import numpy as np
-import sys
-import time
+## TO START ZOOKEEPER AND KAFKA SERVER
+bin/zookeeper-server-start.sh config/zookeeper.properties
+bin/kafka-server-start.sh config/server.properties
 
-DEVICE_PROFILES = {
-    "boston": {'temperature': (0.5,1), 'humidity': (0.5,1), 'pressure': (0.5,1)},
-    "chicago": {'temperature': (0.5,1), 'humidity': (0.5,1), 'pressure': (0.5,1)},
-    "newyork": {'temperature': (0.5,1), 'humidity': (0.5,1), 'pressure': (0.5,1)},
-}
 
-if len(argv)!=2 or argv[1] not in DEVICE_PROFILES:
-    print("please provide a valid device name")
-    for key in DEVICE_PROFILES:
-        print(key)
-    print(f"\nformat: {argv[0]} device_name")
-    exit(1)
+## TO CREATE A TOPIC
+bin/kafka-topics.sh --create -bootstrap-server localhost:9092 --topic weather
 
-profile_name = argv[1]
-profile = DEVICE_PROFILES[profile_name]
+## TO LIST PRESENT TOPICS
+bin/kafka-topics.sh --list --bootstrap-server localhost:9092
 
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
-while True:
-    temp = np.random.normal(profile['temperature'][0], profile['temperature'][1])
-    hum = np.random.normal(profile['humidity'][0], profile['humidity'][1])
-    press = np.random.normal(profile['pressure'][0], profile['pressure'][1])
-    message = f"{profile_name},{temp},{hum},{press}"
-    producer.send('weather', message.encode('utf-8'))
-    print("sending data to kafka")
-    time.sleep(5)
+## START A CONSUMER
+bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic weather --from-beginning
+
+
+## Define .scala file
+## Define .sbt file accordingly
+
+# open a new terminal (get to the desired path which contains the above mentioned scala and sbt files:
+ here it is StreamHandler folder)
+sbt package (will take some time)
+a .jar file will be created in target/scala-2.12/ (this jar file is to be executed)
+
+spark-submit --class StreamHandler --master local[*] --packages "org.apache.kafka-0-10_2.11:3.3.1" target/scala-2.12/streamhandler_2.12-0.1.0-SNAPSHOT.jar
