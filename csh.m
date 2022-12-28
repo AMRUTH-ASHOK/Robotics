0aa93da0390c2319ae99c3a3a7e54d286bb0faaa
Amruth Ashok <BL.EN.U4AIE20002@bl.students.amrita.edu>
2022-12-29 00:48:53 +0530

    Update csh.m

--- csh.m
+++ csh.m
@@ -1,46 +1,99 @@
-global im;
-global im2;
-[path,user_cancel] = imgetfile();
-if user_cancel
-    pass
+CONTRAST_STRETCHING
+x = imread('');
+subplot(1, 2, 1)
+imshow(x);
+y = imadjust(x, stretchlim(x),[]);
+subplot(1, 2, 2)
+imshow(y);
+---
+
+
+gray_image=im2gray(input_image);
+imshow(gray_image)
+darkened_image=0.25*gray_image;
+imshow(darkened_image)
+r_min=min(darkened_image(:))
+r_max=max(darkened_image(:))
+contrast_imge=(darkened_image-r_min).*(255/(r_max-r_min));
+imshow(contrast_imge)
+
+
+HISTOGRAM
+imhist(contrast_imge)
+------
+Img1 = imread('');
+img_hist = zeros(1,256);
+x = 3000; y=3000; 
+for i = 1:x % Rows of Input Image
+    for j = 1:y % Colms  of input image
+        img_hist(Img1(i,j,1)+1) = img_hist(Img1(i,j,1)+1)+1; % Bin Increment for occurrence
+    end
 end
-im = imread(path);
-im2= im2double(im);
-imshow(im2,'Parent',app.UIAxes2)
+figure % Plotting the Histogram
+stem(0:255, img_hist); 
+grid on;
+ylabel('No of Pixels');
+xlabel('GrayScale Intensity Levels'); 
+title('Image Histogram without Using Built-in Function');
 
-## Morphological operations
-se= strel('cube',10);
-c=imclose(a,se);
-d=imopen(a,se);
-e=imerode(a,se);
-f=imdilate(a,se);
+------
+edgedetect = edge(contrast_imge,'sobel');
+Prewitt,Roberts,log,zerocross,sobel,canny
 
 
-## Saturation
-a = imread()
-HSV = rgb2hsv(a)
-[h, s, v] = imsplit(HSV)
-saturation_factor = 2
-S_saturation = s*saturation_factor
-hsv_saturation = cat(3, h, S_saturation, v)
-Saturated = rgb2hsv(hsv_saturation)
-imshow(Saturated)
 
-## Kmeans Segmentation
-[L,Centers] = imsegkmeans(I,4);
-B = labeloverlay(I,L);
-imshow(B)
+-----
 
-## MultiLevel Thresholing
-level = multithresh(I);
-seg_I = imquantize(I,level);
+Iblur1 = imgaussfilt(I,2);
 
-thresh = multithresh(I,2);
-seg_I = imquantize(I,thresh);
-RGB = label2rgb(seg_I); 
+%normal matrix : [1 0 0; 0 1 0; 0 0 1]
+theta = 180
+rotation = [cos(theta) -sin(theta) 0; sin(theta) cos(theta) 0; 0 0 1]; %rotate based on matrix  [cost, -sint; sint, cost]
+tform1 = affine2d(rotation)
+scale = [2 0 0; 0 4 0; 0 0 1]; %keep 0's same, scale up or down the values of 1's
+tform2 = affine2d(scale)
+reflection  = [1 0 0; 0 -1 0; 0 0 1]; %keep 0's same, but make the 1's value negative
+tform3 = affine2d(reflection)
+shearr = [1 0.33 0; 0 1 0; 0 0 1]; %keep 1's same, change 0
+tform4 = affine2d(shearr)
+O = imtranslate(I,[100, 400]);
+J = imwarp(I,tform1);
+H = imwarp(I,tform3);
 
-## Grey Thres
-level = graythresh(I)
-BW = imbinarize(I,level);
+-----
+THRESHOLDING
+x = imread('');
+x = rgb2gray(x);
+x = double(x);
+tot = 0;
+[a,b] = size(x);
+y = zeros(a,b);
+for i=1:a
+    for j=1:b
+        y(i,j) = 0;
+    end
+end
+for i=1:a
+    for j=1:b
+        tot = tot + x(i,j);
+    end
+end
+c = a*b;
+thr = tot/(a*b);
+for i=1:a
+    for j=1:b
+        if x(i,j) > thr
+            y(i,j)=1;
+        else
+            y(i,j)=0;
+        end
+    end
+end
+figure;
+imshow(y);
 
-	 
+
+
+
+
+
